4717918e8f665ea8cfdebaaf7351b081baf38945
zdyxry <zdyxry@gmail.com>
2026-04-27 18:07:18 +0800

    chore: update dependencies and components

--- src/pages/og/[...slug].png.ts
+++ src/pages/og/[...slug].png.ts
@@ -10,7 +10,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     const year = String(date.getFullYear());
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    const slugPart = post.data.slug || post.slug;
+    const slugPart = post.data.slug || post.id;
     const fullSlug = `${year}/${month}/${day}/${slugPart}`;
 
     return {
